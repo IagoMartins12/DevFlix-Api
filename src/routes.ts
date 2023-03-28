57cab5130a59839e94887ab7f9122d9279f6fe6c
Iago saraiva <iago.saraiva@pmovil.com.br>
2023-03-28 16:07:19 -0300

    user authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import express  from "express"
+import { authController } from "./controllers/authController"
 import { categoriesController } from "./controllers/categorieController"
 import { coursesController } from "./controllers/coursesController"
 import { episodesController } from "./controllers/episodesController"
 
 const router = express.Router()
+
+//auth endpoints
+router.post('/auth/register', authController.register)
 
 //categories endpoints
 router.get('/categories', categoriesController.index)
@@ -15,5 +19,6 @@ router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
+//episodes endpoints 
 router.get('/episodes/stream', episodesController.stream)
 export { router }
--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,13 @@ const app = express()
 //Indicando arquivos estaticos
 app.use(express.static('public'))
 
+app.use(express.json())
+
 //app.use(caminho, rotas)
 app.use(adminJs.options.rootPath, adminJsRouter)
 
 app.use(router)
+
 
 const PORT = process.env.port || 3000
 
